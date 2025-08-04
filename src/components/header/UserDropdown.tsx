89a3908a1de6cce0713afebb09afe00c01f55929
Muhammad Hassan <akramhassan4445@gmail.com>
2025-08-04 03:34:39 -0700

    Remove user profile from drop down

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -81,7 +81,9 @@ export default function UserDropdown({ userName, userEmail, userImageSrc }: User
           </span>
         </div>
 
-        <ul className="flex flex-col gap-1 pt-4 pb-3 border-b border-gray-200 dark:border-gray-800">
+      
+          {/* SECTION - DROP DOWN OF USER PROFILE */}
+        {/* <ul className="flex flex-col gap-1 pt-4 pb-3 border-b border-gray-200 dark:border-gray-800">
           <li>
             <DropdownItem
               onItemClick={closeDropdown}
@@ -107,7 +109,7 @@ export default function UserDropdown({ userName, userEmail, userImageSrc }: User
               Edit profile
             </DropdownItem>
           </li>
-        </ul>
+        </ul> */}
         <Button
           onClick={() => {localStorage.clear();  window.location.href = '/login'; }} // Assuming this is your sign out route
           className="flex items-center gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
